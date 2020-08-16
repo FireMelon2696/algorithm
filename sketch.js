4c2d93fb1b4b1d1b184de574375dece052f42a5b
FireMelon2696 <69468124+FireMelon2696@users.noreply.github.com>
2020-08-16 12:28:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,27 @@
 var box1,box2;
 function setup() {
-  createCanvas(800,400);
-  box1 = createSprite(400, 200, 50, 50);
-  box2 = createSprite(200,200,50,50);
+  createCanvas(800,800);
+  box1 = createSprite(750, 750, 50, 50);
+  box2 = createSprite(50,50,50,50);
   box1.shapeColor = "lightblue";
   box2.shapeColor = "darkblue";
-
+  box1.velocityX = -1;
+  box1.velocityY = -1;
+  box2.velocityX = 1;
+  box2.velocityY = 1;
 }
 
 function draw() {
   background(255,255,255);
-  box1.x=mouseX;
-  box1.y=mouseY;
-  if(box1.x-box2.x<box1.width/2+box2.width/2 && box2.x-box1.x<box1.width/2+box2.width/2
-    && box1.y-box2.y<box1.height/2+box2.height/2 && box2.y-box1.y<box1.height/2+box2.height/2){
+ // box1.x=mouseX;
+ // box1.y=mouseY;
+  if(isTouching(box1,box2)){
       box1.shapeColor = "red";
       box2.shapeColor = "green";
-    }else{
-      box1.shapeColor = "lightblue";
-      box2.shapeColor = "darkblue";
+    //}else{
+     // box1.shapeColor = "lightblue";
+    //  box2.shapeColor = "darkblue";
     }
+    bounceOff(box1,box2);
   drawSprites();
 }
